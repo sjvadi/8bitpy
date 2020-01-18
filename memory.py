ea63fb01711e6ea79ad8f25c9619da8cb73e076b
sjvadi <sjoerdmcba@gmail.com>
2020-01-18 13:45:13 +0100

    added an 8 bit memory bys

---
+++ memory.py
@@ -0,0 +1,11 @@
+class Memory:
+    mem_bus = {
+            0 : False,
+            1 : False,
+            10 : False,
+            11 : False,
+            100 : False,
+            101 : False,
+            110 : False,
+            1000: False
+            }
